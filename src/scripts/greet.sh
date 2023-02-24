40c92b505bba728373c731bba8b63829c34c92df
Al Clark <alanjclark@gmail.com>
2023-02-24 14:54:38 +0000

    asdasd

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # echo Hello "${PARAM_TO}"
 # echo "<< pipeline.id >>"
-curl -G "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID" -H "circle-token: $CIRCLE_TOKEN" | jq -r '.pipeline_id'
+curl -G "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID" | jq -r '.pipeline_id'
