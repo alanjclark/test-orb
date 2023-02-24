3d20f868c11256526ecf6e3640a4716612aa793f
Al Clark <alanjclark@gmail.com>
2023-02-24 14:42:12 +0000

    trying API gubbins

--- src/scripts/greet.sh
+++ src/scripts/greet.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # echo Hello "${PARAM_TO}"
-echo "<< pipeline.id >>"
+# echo "<< pipeline.id >>"
+curl -G "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID" -H "circle-token: $CIRCLE_TOKEN" | jq -r '.pipeline_id'
